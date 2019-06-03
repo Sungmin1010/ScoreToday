fed7f1d9d76795eeeef772d356f08cafe936c811
Sungmin-PC <lmsgsm1@gmail.com>
2019-06-04 01:20:28 +0900

    UserDAO 테스트 작성

---
+++ ScoreToday/src/test/java/com/sct/dao/UserDAOImplTest.java
@@ -0,0 +1,64 @@
+package com.sct.dao;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import javax.inject.Inject;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.sct.vo.UserVO;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
+public class UserDAOImplTest {
+	
+	@Inject
+	private UserDAO dao;
+	
+	private UserVO vo;
+	
+	@Before
+	public void setUp() throws Exception{
+		//insert test data
+		this.vo = new UserVO();
+		vo.setId("lmsg");
+		vo.setName("¿”º∫πŒ");
+		vo.setEmail("lmsgsm1@test.com");
+		vo.setPassword("1234");
+		//vo.setJoindate(new Date());
+		
+		dao.insertUser(vo);
+		
+	}
+
+	@Test
+	public void testGetUser() throws Exception {
+		UserVO getVO = dao.getUser(vo);
+		assertEquals(getVO.getId(), this.vo.getId());
+		assertEquals(getVO.getName(), this.vo.getName());
+		assertEquals(getVO.getEmail(), this.vo.getEmail());
+		assertEquals(getVO.getPassword(), this.vo.getPassword());
+		//assertEquals(getVO.getJoindate(), this.vo.getJoindate());
+		
+	}
+	
+	@Test
+	public void testInsertUser() throws Exception {
+		
+		
+	}
+	
+	@After
+	public void tearDown() throws Exception{
+		dao.deleteUser(vo);
+	}
+
+}
