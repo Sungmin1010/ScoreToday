24cb44ce1fa3fb42ff05caab746476f780841c9b
Sungmin-PC <lmsgsm1@gmail.com>
2019-07-01 23:54:16 +0900

    logger 메세지 추가

--- ScoreToday/src/main/java/com/sct/controller/UserController.java
+++ ScoreToday/src/main/java/com/sct/controller/UserController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -23,8 +24,10 @@ public class UserController {
 	
 	@RequestMapping(value="/login", method=RequestMethod.POST)
 	public String login(Model model, UserVO vo, RedirectAttributes rttr) throws Exception {
+		logger.info("입력 vo : "+ vo.toString());
 		UserVO result = service.login(vo);
 		if(result==null) {
+			logger.info("존재하지 않는 회원정보");
 			rttr.addFlashAttribute("msg", "fail");
 			return "redirect:/";
 		}
