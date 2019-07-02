d8ccf130c3057a2c83a922be2b722273e67abd4b
Sungmin-PC <lmsgsm1@gmail.com>
2019-07-02 16:30:10 +0900

    스텁을 이용해 리다이렉션 상황 테스트

--- ScoreToday/src/test/java/com/sct/controller/UserControllerStandaloneTest.java
+++ ScoreToday/src/test/java/com/sct/controller/UserControllerStandaloneTest.java
@@ -1,10 +1,14 @@
 package com.sct.controller;
 
 import static org.hamcrest.CoreMatchers.anything;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.flash;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
@@ -26,7 +30,6 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import com.sct.service.UserService;
 import com.sct.vo.UserVO;
 
-//@RunWith(SpringJUnit4ClassRunner.class) //MockitoJUnitRunner.class
 public class UserControllerStandaloneTest {
 	
 	@Mock
@@ -40,7 +43,6 @@ public class UserControllerStandaloneTest {
 	@Before
 	public void setup() throws Exception{
 		MockitoAnnotations.initMocks(this);
-		
 		
 		InternalResourceViewResolver internalResourceViewResolver = new	InternalResourceViewResolver();
 		internalResourceViewResolver.setPrefix("/WEB-INF/views/");
@@ -65,5 +67,19 @@ public class UserControllerStandaloneTest {
 		.andExpect(status().isOk())
 		.andExpect(view().name("main"));
 	}
+	
+	@Test
+	public void testLogin_WorngIdOrPassword() throws Exception {
+		//make stub : return userVO is null
+		when(userService.login(any(UserVO.class))).thenReturn(any(UserVO.class));
+		
+		mockMvc.perform(post("/login").param("id", "wrongid").param("password", "abcd1234"))
+		.andDo(print())
+		.andExpect(status().is3xxRedirection()) //redirection is not OK status
+		.andExpect(redirectedUrl("/"))
+		.andExpect(flash().attribute("msg", "fail"));
+	}
+	
+	
 
 }
