e145921d43ba9451758dbc11fc316b1b2e37f05f
Sungmin-PC <lmsgsm1@gmail.com>
2019-07-18 02:15:25 +0900

    Repository 빈(ScoreDAOImpl) 등록 오류 수정

--- ScoreToday/src/main/java/com/sct/dao/ScoreDAOImpl.java
+++ ScoreToday/src/main/java/com/sct/dao/ScoreDAOImpl.java
@@ -1,11 +1,21 @@
 package com.sct.dao;
 
+
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
 import com.sct.vo.ScoreVO;
 
 @Repository
 public class ScoreDAOImpl implements ScoreDAO {
+	
+	@Inject
+	private SqlSession session;
+	
+	private static final String namespace = "com.sct.mapper.ScoreMapper";
 
 	@Override
 	public void insertScore(ScoreVO vo) {
--- ScoreToday/src/test/java/com/sct/dao/ScoreDAOImplTest.java
+++ ScoreToday/src/test/java/com/sct/dao/ScoreDAOImplTest.java
@@ -6,6 +6,7 @@ import javax.inject.Inject;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,14 +14,13 @@ import org.springframework.transaction.annotation.Transactional;
 import com.sct.vo.ScoreVO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"file:src/main/webapp/WBE-INF/spring/**/root-context.xml"})
-
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
 public class ScoreDAOImplTest {
 	
 	@Inject
 	private ScoreDAO dao;
 	
-	@Transactional
+	//@Transactional
 	@Test
 	public void testInsertScore() throws Exception {
 		//given : 선행조건 기술
