709982bb67848a02686785a3f5a0390279c1a4a0
Sungmin-PC <lmsgsm1@gmail.com>
2019-07-26 17:21:07 +0900

    post score 테스트 추가

--- ScoreToday/src/main/java/com/sct/controller/ScoreController.java
+++ ScoreToday/src/main/java/com/sct/controller/ScoreController.java
@@ -1,5 +1,7 @@
 package com.sct.controller;
 
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -8,10 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.sct.service.ScoreService;
+
 @Controller
 public class ScoreController {
 
 	private static final Logger logger = LoggerFactory.getLogger(ScoreController.class);
+	
+	@Inject
+	private ScoreService service;
 	
 	@RequestMapping(value="/main/record", method=RequestMethod.GET)
 	public String getRecord(@RequestParam(name="timecategory", defaultValue="x", required=false) String timecategory, Model model)throws Exception{
@@ -20,6 +27,12 @@ public class ScoreController {
 		model.addAttribute("timecategory", timecategory);
 		
 		return "main/record";
+	}
+	
+	@RequestMapping(value="/main/record", method=RequestMethod.POST)
+	public String postRecord()throws Exception{
+		//TODO : 리팩토링 & 테스트 부분에 서비스 메소드 verify 코드 추가
+		return "main";
 	}
 
 	private String makeTimeCategory(char timecategory) {
--- ScoreToday/src/test/java/com/sct/controller/ScoreControllerTest.java
+++ ScoreToday/src/test/java/com/sct/controller/ScoreControllerTest.java
@@ -1,8 +1,12 @@
 package com.sct.controller;
 
+import static org.hamcrest.CoreMatchers.any;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -10,31 +14,40 @@ import javax.inject.Inject;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import com.sct.service.ScoreService;
+import com.sct.vo.ScoreVO;
+import com.sct.vo.UserInfoVO;
+import com.sct.vo.UserVO;
 
 public class ScoreControllerTest {
 	
-	//@Mock
-	//private ScoreService scoreService;
+	@Mock
+	private ScoreService scoreService;
 	
-	//@Mock
-	//private ScoreController scoreController;
+	@InjectMocks
+	private ScoreController scoreController;
 	
 	private MockMvc mockMvc;
 	
 	
 	@Before
 	public void setup() throws Exception{
+		MockitoAnnotations.initMocks(this);
+		
 		InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
 		internalResourceViewResolver.setPrefix("/WEB-INF/views/");
 		internalResourceViewResolver.setSuffix(".jsp");
 		
-		this.mockMvc = MockMvcBuilders.standaloneSetup(new ScoreController())
+		this.mockMvc = MockMvcBuilders.standaloneSetup(scoreController)
 				.setViewResolvers(internalResourceViewResolver).build();
 	}
 	
@@ -49,6 +62,26 @@ public class ScoreControllerTest {
 				.andExpect(model().attribute("timecategory", is("AM")))
 				.andExpect(view().name("main/record"));
 		
+	}
+	
+	@Test
+	public void testPostRecord() throws Exception {
+		//given : record bodyscore:10, mindscore10, mentalscore10, timecategory:a, useq:1
+		//when : request /main/record POST
+		//then : status OK, view main/main
+		ScoreVO mockScoreVo = mock(ScoreVO.class);
+		UserInfoVO mockUserInfo = mock(UserInfoVO.class);
+		//when(scoreService.addScore(any(ScoreVO.class))).thenReturn
+		RequestBuilder reqBuilder = MockMvcRequestBuilders.post("/main/record")
+														.requestAttr("score", mockScoreVo)
+														.sessionAttr("userInfo", mockUserInfo);
+		mockMvc.perform(reqBuilder)
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(view().name(is("main")));
+		
+		//verify(scoreService).addScore();
+				
 	}
 
 }
