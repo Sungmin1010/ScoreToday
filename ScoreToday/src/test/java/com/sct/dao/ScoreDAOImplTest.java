740c5d7c01ea3f3abc23c7d1122e6bc084d89b58
Sungmin-PC <lmsgsm1@gmail.com>
2019-08-24 00:43:58 +0900

    메인화면에서 입력 점수의 상태를 출력하기 위한 dao 테스트 작성(ST003-03)

--- ScoreToday/src/test/java/com/sct/dao/ScoreDAOImplTest.java
+++ ScoreToday/src/test/java/com/sct/dao/ScoreDAOImplTest.java
@@ -1,6 +1,11 @@
 package com.sct.dao;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -12,6 +17,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.sct.vo.ScoreVO;
+import com.sct.vo.UserDayScoreVO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
@@ -37,6 +43,24 @@ public class ScoreDAOImplTest {
 		assertEquals(dao.countScore(), beforeCount+1);
 		
 		
+	}
+	
+	@Test
+	public void testSelectThreeScore() throws Exception{
+		//given : 선행조건 기술
+		//useq가 1인 사용자의 2019년 8월 20일 입력한 점수가 2개 존재한다.
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = formatter.parse("2019-08-20");
+		UserDayScoreVO vo = new UserDayScoreVO();
+		vo.setUseq(1);
+		vo.setScoredate(date);
+		//when : 기능 수행
+		List<ScoreVO> scoreList = dao.selectDayScore(vo);
+		
+		//then : 결과 확인
+		assertThat(scoreList.size(), is(2));
+		assertThat(scoreList.get(0).getBodyscore(), is(10));
+		assertThat(scoreList.get(1).getBodyscore(), is(8));
 	}
 
 }
